b62f52def237a0a0313710cb825dff057458152d
Template Janitor <template.janitor@example.com>
2026-03-09 14:57:53 +0000

    Cleanup template and initialize repository

--- ProjectOptions.cmake
+++ ProjectOptions.cmake
@@ -7,7 +7,7 @@ include(CheckCXXCompilerFlag)
 include(CheckCXXSourceCompiles)
 
 
-macro(myproject_supports_sanitizers)
+macro(MyGLViewer_supports_sanitizers)
   # Emscripten doesn't support sanitizers
   if(EMSCRIPTEN)
     set(SUPPORTS_UBSAN OFF)
@@ -58,110 +58,110 @@ macro(myproject_supports_sanitizers)
   endif()
 endmacro()
 
-macro(myproject_setup_options)
-  option(myproject_ENABLE_HARDENING "Enable hardening" ON)
-  option(myproject_ENABLE_COVERAGE "Enable coverage reporting" OFF)
+macro(MyGLViewer_setup_options)
+  option(MyGLViewer_ENABLE_HARDENING "Enable hardening" ON)
+  option(MyGLViewer_ENABLE_COVERAGE "Enable coverage reporting" OFF)
   cmake_dependent_option(
-    myproject_ENABLE_GLOBAL_HARDENING
+    MyGLViewer_ENABLE_GLOBAL_HARDENING
     "Attempt to push hardening options to built dependencies"
     ON
-    myproject_ENABLE_HARDENING
+    MyGLViewer_ENABLE_HARDENING
     OFF)
 
-  myproject_supports_sanitizers()
+  MyGLViewer_supports_sanitizers()
 
-  if(NOT PROJECT_IS_TOP_LEVEL OR myproject_PACKAGING_MAINTAINER_MODE)
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" OFF)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" OFF)
+  if(NOT PROJECT_IS_TOP_LEVEL OR MyGLViewer_PACKAGING_MAINTAINER_MODE)
+    option(MyGLViewer_ENABLE_IPO "Enable IPO/LTO" OFF)
+    option(MyGLViewer_WARNINGS_AS_ERRORS "Treat Warnings As Errors" OFF)
+    option(MyGLViewer_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" OFF)
+    option(MyGLViewer_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyGLViewer_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" OFF)
+    option(MyGLViewer_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyGLViewer_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyGLViewer_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyGLViewer_ENABLE_CLANG_TIDY "Enable clang-tidy" OFF)
+    option(MyGLViewer_ENABLE_CPPCHECK "Enable cpp-check analysis" OFF)
+    option(MyGLViewer_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyGLViewer_ENABLE_CACHE "Enable ccache" OFF)
   else()
-    option(myproject_ENABLE_IPO "Enable IPO/LTO" ON)
-    option(myproject_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
-    option(myproject_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
-    option(myproject_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
-    option(myproject_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
-    option(myproject_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
-    option(myproject_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
-    option(myproject_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
-    option(myproject_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
-    option(myproject_ENABLE_PCH "Enable precompiled headers" OFF)
-    option(myproject_ENABLE_CACHE "Enable ccache" ON)
+    option(MyGLViewer_ENABLE_IPO "Enable IPO/LTO" ON)
+    option(MyGLViewer_WARNINGS_AS_ERRORS "Treat Warnings As Errors" ON)
+    option(MyGLViewer_ENABLE_SANITIZER_ADDRESS "Enable address sanitizer" ${SUPPORTS_ASAN})
+    option(MyGLViewer_ENABLE_SANITIZER_LEAK "Enable leak sanitizer" OFF)
+    option(MyGLViewer_ENABLE_SANITIZER_UNDEFINED "Enable undefined sanitizer" ${SUPPORTS_UBSAN})
+    option(MyGLViewer_ENABLE_SANITIZER_THREAD "Enable thread sanitizer" OFF)
+    option(MyGLViewer_ENABLE_SANITIZER_MEMORY "Enable memory sanitizer" OFF)
+    option(MyGLViewer_ENABLE_UNITY_BUILD "Enable unity builds" OFF)
+    option(MyGLViewer_ENABLE_CLANG_TIDY "Enable clang-tidy" ON)
+    option(MyGLViewer_ENABLE_CPPCHECK "Enable cpp-check analysis" ON)
+    option(MyGLViewer_ENABLE_PCH "Enable precompiled headers" OFF)
+    option(MyGLViewer_ENABLE_CACHE "Enable ccache" ON)
   endif()
 
   if(NOT PROJECT_IS_TOP_LEVEL)
     mark_as_advanced(
-      myproject_ENABLE_IPO
-      myproject_WARNINGS_AS_ERRORS
-      myproject_ENABLE_SANITIZER_ADDRESS
-      myproject_ENABLE_SANITIZER_LEAK
-      myproject_ENABLE_SANITIZER_UNDEFINED
-      myproject_ENABLE_SANITIZER_THREAD
-      myproject_ENABLE_SANITIZER_MEMORY
-      myproject_ENABLE_UNITY_BUILD
-      myproject_ENABLE_CLANG_TIDY
-      myproject_ENABLE_CPPCHECK
-      myproject_ENABLE_COVERAGE
-      myproject_ENABLE_PCH
-      myproject_ENABLE_CACHE)
+      MyGLViewer_ENABLE_IPO
+      MyGLViewer_WARNINGS_AS_ERRORS
+      MyGLViewer_ENABLE_SANITIZER_ADDRESS
+      MyGLViewer_ENABLE_SANITIZER_LEAK
+      MyGLViewer_ENABLE_SANITIZER_UNDEFINED
+      MyGLViewer_ENABLE_SANITIZER_THREAD
+      MyGLViewer_ENABLE_SANITIZER_MEMORY
+      MyGLViewer_ENABLE_UNITY_BUILD
+      MyGLViewer_ENABLE_CLANG_TIDY
+      MyGLViewer_ENABLE_CPPCHECK
+      MyGLViewer_ENABLE_COVERAGE
+      MyGLViewer_ENABLE_PCH
+      MyGLViewer_ENABLE_CACHE)
   endif()
 
-  myproject_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
-  if(LIBFUZZER_SUPPORTED AND (myproject_ENABLE_SANITIZER_ADDRESS OR myproject_ENABLE_SANITIZER_THREAD OR myproject_ENABLE_SANITIZER_UNDEFINED))
+  MyGLViewer_check_libfuzzer_support(LIBFUZZER_SUPPORTED)
+  if(LIBFUZZER_SUPPORTED AND (MyGLViewer_ENABLE_SANITIZER_ADDRESS OR MyGLViewer_ENABLE_SANITIZER_THREAD OR MyGLViewer_ENABLE_SANITIZER_UNDEFINED))
     set(DEFAULT_FUZZER ON)
   else()
     set(DEFAULT_FUZZER OFF)
   endif()
 
-  option(myproject_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
+  option(MyGLViewer_BUILD_FUZZ_TESTS "Enable fuzz testing executable" ${DEFAULT_FUZZER})
 
 endmacro()
 
-macro(myproject_global_options)
-  if(myproject_ENABLE_IPO)
+macro(MyGLViewer_global_options)
+  if(MyGLViewer_ENABLE_IPO)
     include(cmake/InterproceduralOptimization.cmake)
-    myproject_enable_ipo()
+    MyGLViewer_enable_ipo()
   endif()
 
-  myproject_supports_sanitizers()
+  MyGLViewer_supports_sanitizers()
 
-  if(myproject_ENABLE_HARDENING AND myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyGLViewer_ENABLE_HARDENING AND MyGLViewer_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyGLViewer_ENABLE_SANITIZER_UNDEFINED
+       OR MyGLViewer_ENABLE_SANITIZER_ADDRESS
+       OR MyGLViewer_ENABLE_SANITIZER_THREAD
+       OR MyGLViewer_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    message("${myproject_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${myproject_ENABLE_SANITIZER_UNDEFINED}")
-    myproject_enable_hardening(myproject_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    message("${MyGLViewer_ENABLE_HARDENING} ${ENABLE_UBSAN_MINIMAL_RUNTIME} ${MyGLViewer_ENABLE_SANITIZER_UNDEFINED}")
+    MyGLViewer_enable_hardening(MyGLViewer_options ON ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 endmacro()
 
-macro(myproject_local_options)
+macro(MyGLViewer_local_options)
   if(PROJECT_IS_TOP_LEVEL)
     include(cmake/StandardProjectSettings.cmake)
   endif()
 
-  add_library(myproject_warnings INTERFACE)
-  add_library(myproject_options INTERFACE)
+  add_library(MyGLViewer_warnings INTERFACE)
+  add_library(MyGLViewer_options INTERFACE)
 
   include(cmake/CompilerWarnings.cmake)
-  myproject_set_project_warnings(
-    myproject_warnings
-    ${myproject_WARNINGS_AS_ERRORS}
+  MyGLViewer_set_project_warnings(
+    MyGLViewer_warnings
+    ${MyGLViewer_WARNINGS_AS_ERRORS}
     ""
     ""
     ""
@@ -172,66 +172,66 @@ macro(myproject_local_options)
 
   if(NOT EMSCRIPTEN)
     include(cmake/Sanitizers.cmake)
-    myproject_enable_sanitizers(
-      myproject_options
-      ${myproject_ENABLE_SANITIZER_ADDRESS}
-      ${myproject_ENABLE_SANITIZER_LEAK}
-      ${myproject_ENABLE_SANITIZER_UNDEFINED}
-      ${myproject_ENABLE_SANITIZER_THREAD}
-      ${myproject_ENABLE_SANITIZER_MEMORY})
+    MyGLViewer_enable_sanitizers(
+      MyGLViewer_options
+      ${MyGLViewer_ENABLE_SANITIZER_ADDRESS}
+      ${MyGLViewer_ENABLE_SANITIZER_LEAK}
+      ${MyGLViewer_ENABLE_SANITIZER_UNDEFINED}
+      ${MyGLViewer_ENABLE_SANITIZER_THREAD}
+      ${MyGLViewer_ENABLE_SANITIZER_MEMORY})
   endif()
 
-  set_target_properties(myproject_options PROPERTIES UNITY_BUILD ${myproject_ENABLE_UNITY_BUILD})
+  set_target_properties(MyGLViewer_options PROPERTIES UNITY_BUILD ${MyGLViewer_ENABLE_UNITY_BUILD})
 
-  if(myproject_ENABLE_PCH)
+  if(MyGLViewer_ENABLE_PCH)
     target_precompile_headers(
-      myproject_options
+      MyGLViewer_options
       INTERFACE
       <vector>
       <string>
       <utility>)
   endif()
 
-  if(myproject_ENABLE_CACHE)
+  if(MyGLViewer_ENABLE_CACHE)
     include(cmake/Cache.cmake)
-    myproject_enable_cache()
+    MyGLViewer_enable_cache()
   endif()
 
   include(cmake/StaticAnalyzers.cmake)
-  if(myproject_ENABLE_CLANG_TIDY)
-    myproject_enable_clang_tidy(myproject_options ${myproject_WARNINGS_AS_ERRORS})
+  if(MyGLViewer_ENABLE_CLANG_TIDY)
+    MyGLViewer_enable_clang_tidy(MyGLViewer_options ${MyGLViewer_WARNINGS_AS_ERRORS})
   endif()
 
-  if(myproject_ENABLE_CPPCHECK)
-    myproject_enable_cppcheck(${myproject_WARNINGS_AS_ERRORS} "" # override cppcheck options
+  if(MyGLViewer_ENABLE_CPPCHECK)
+    MyGLViewer_enable_cppcheck(${MyGLViewer_WARNINGS_AS_ERRORS} "" # override cppcheck options
     )
   endif()
 
-  if(myproject_ENABLE_COVERAGE)
+  if(MyGLViewer_ENABLE_COVERAGE)
     include(cmake/Tests.cmake)
-    myproject_enable_coverage(myproject_options)
+    MyGLViewer_enable_coverage(MyGLViewer_options)
   endif()
 
-  if(myproject_WARNINGS_AS_ERRORS)
+  if(MyGLViewer_WARNINGS_AS_ERRORS)
     check_cxx_compiler_flag("-Wl,--fatal-warnings" LINKER_FATAL_WARNINGS)
     if(LINKER_FATAL_WARNINGS)
       # This is not working consistently, so disabling for now
-      # target_link_options(myproject_options INTERFACE -Wl,--fatal-warnings)
+      # target_link_options(MyGLViewer_options INTERFACE -Wl,--fatal-warnings)
     endif()
   endif()
 
-  if(myproject_ENABLE_HARDENING AND NOT myproject_ENABLE_GLOBAL_HARDENING)
+  if(MyGLViewer_ENABLE_HARDENING AND NOT MyGLViewer_ENABLE_GLOBAL_HARDENING)
     include(cmake/Hardening.cmake)
     if(NOT SUPPORTS_UBSAN 
-       OR myproject_ENABLE_SANITIZER_UNDEFINED
-       OR myproject_ENABLE_SANITIZER_ADDRESS
-       OR myproject_ENABLE_SANITIZER_THREAD
-       OR myproject_ENABLE_SANITIZER_LEAK)
+       OR MyGLViewer_ENABLE_SANITIZER_UNDEFINED
+       OR MyGLViewer_ENABLE_SANITIZER_ADDRESS
+       OR MyGLViewer_ENABLE_SANITIZER_THREAD
+       OR MyGLViewer_ENABLE_SANITIZER_LEAK)
       set(ENABLE_UBSAN_MINIMAL_RUNTIME FALSE)
     else()
       set(ENABLE_UBSAN_MINIMAL_RUNTIME TRUE)
     endif()
-    myproject_enable_hardening(myproject_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
+    MyGLViewer_enable_hardening(MyGLViewer_options OFF ${ENABLE_UBSAN_MINIMAL_RUNTIME})
   endif()
 
 endmacro()
--- src/sample_library/sample_library.cpp
+++ src/sample_library/sample_library.cpp
@@ -1,4 +1,4 @@
-#include <myproject/sample_library.hpp>
+#include <MyGLViewer/sample_library.hpp>
 
 int factorial(int input) noexcept
 {
